e2a965d69e8e2f6cd9798241c4a37489091b8598
Mackenzie Martin <mmartin@smith.edu>
2024-10-30 16:13:54 -0400

    Creating a stub for the station class.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,5 +3,20 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
   end
-  
+
+  def has_bike(bike_id)
+
+  end
+
+  def checkout_bike(bike_id)
+
+  end
+
+  def return_bike(bike_id)
+
+  end
+
+  def get_list_of_bikes(station_id)
+
+  end
 end
