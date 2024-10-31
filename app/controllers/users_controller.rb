c4c80af215ffc79b3ef1a0ae99547ed12f5a3f72
mneedham01 <mneedham@smith.edu>
2024-10-31 15:55:50 -0400

    Coded verify_account method within Users controller -- should not be final place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
-  def verify_account
+  def verify_account(userName, password)
+    # if userName is not in database
+    if User.where(userName: userName).count == 0
+      false
+    # userName is in database 
+    else 
+      # find user password associated 
+      user = User.where(userName: userName)[0]
+      # if the password is not the same as the one in the database 
+      if user.password != password 
+        false
+      end 
+    # if the userName is in the database and the password matches, then all good! 
+    true 
   end
 
   def delete_account
