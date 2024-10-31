e756e402811dce31cd436cc4b0f20811a7df5494
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-10-31 14:34:51 -0400

    Created User Model and User Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_090719) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_31_182235) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_090719) do
     t.integer "identifier"
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "userName"
+    t.string "password"
+    t.boolean "loginStatus"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
