5d0f32ddd5954f8cf0ba0139c00eb701134e5cf0
mariaigartua <emaildemajo@gmail.com>
2024-11-02 16:49:49 -0400

    Created Bike class and getters

--- config/routes.rb
+++ config/routes.rb
@@ -7,30 +7,10 @@ Rails.application.routes.draw do
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
+  
   get 'users/verify_account'
   get 'users/delete_account'
-  get 'trip/index'
-  get 'trip/book'
-  get 'trip/confirm'
   
-  root to: "stations#index"
-
-  get "stations/show"
-  get "stations/index"
-
-  
-
-
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bikes/edit'
-  get 'bikes/delete'
-  get 'bikes/new'
-  
-
-#developed this new route to use it later in the html page
-#so that we could display the bikes and the stations in different view pages
-
-
+  root to: "home#main"
 
 end
