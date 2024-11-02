9084d8a1720312e3b24bd174871afd9d9de4d0cf
mneedham01 <mneedham@smith.edu>
2024-11-02 14:26:07 -0400

    Create controller for Director class with methods login, register, and verify_email.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
+  get 'directors/login'
+  get 'directors/register'
+  get 'directors/verify_email'
   get 'users/verify_account'
   get 'users/delete_account'
   root to: "stations#index"
