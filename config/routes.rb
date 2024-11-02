f40b10da335a67cc6a727cab3b986441a39622f7
mariaigartua <emaildemajo@gmail.com>
2024-11-02 16:49:49 -0400

    Created Bike class and getters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'bike/get_bike_id'
+  get 'bike/get_station_id'
+  get 'bike/get_station_name'
+  get 'bike/get_battery_percentage'
   get 'users/verify_account'
   get 'users/delete_account'
   root to: "stations#index"
