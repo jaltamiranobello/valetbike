a38cf7fae58076448569b7aabc9390caa45f0c6e
mariaigartua <emaildemajo@gmail.com>
2024-11-02 16:49:49 -0400

    Created Bike class and getters

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  get 'users/show'
-  
-  get 'card/remove_card'
-  get 'card/update_payment'
-  get 'card/verify_payment'
-  get 'card/pay'
   get 'bike/get_bike_id'
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
+<<<<<<< HEAD
   
   root to: "home#main"
 end
+=======
+  get 'users/verify_account'
+  get 'users/delete_account'
+  root to: "stations#index"
+end
+>>>>>>> 79cb473 (Created Bike class and getters)
