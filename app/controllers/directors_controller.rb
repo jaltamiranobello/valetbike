3644f05537c448c43300def9785fe2e89f5f1041
mneedham01 <mneedham@smith.edu>
2024-11-03 11:08:18 -0500

    Created base for register method and added comments to login method

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,11 +13,27 @@ class DirectorsController < ApplicationController
         false
   end
 
-  def register
+  def register(firstName, lastName, email, password)
+    # Check if the email is in the database
+    if email_in_database(email)
+      puts "Customer already registered"
+      false
+    end
+    # Email is not already in database 
+    else 
+      # Create new Customer
+      # To do: Check validity of first name 
+      # To do: Check validity of last name 
+      # To do: Check validity of email 
+      # To do: Check validity of password 
+      Customer.create(firstName: firstName, lastName: lastName, email: email, password: password)
+      # Return true because the registration was successful
+      true
+    end 
   end
 
   def email_in_database(email)
-    #Checks Customer database and returns true if it exits otherwise false
+    # Checks Customer database and returns true if it exits otherwise false
     inDatabase = Customer.where(email: email).first != nil ? true : false
   end
 end
