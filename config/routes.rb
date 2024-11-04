e5f05d9d11aff636e62f7c1d32ac43e59acd2b9b
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-11-04 17:06:54 -0500

    Update the website root path

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   get 'bike/get_battery_percentage'
   get 'users/verify_account'
   get 'users/delete_account'
-  root to: "stations#index"
+  #root to: "stations#index"
+  root to: "directors#new"
 end
