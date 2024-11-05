b6be99204e604ff19715014efd13608672b7df50
mneedham01 <mneedham@smith.edu>
2024-11-04 21:59:33 -0500

    added methods for registering a new Customer

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,51 +1,11 @@
 class DirectorsController < ApplicationController
 
+  # the view for this action displays the form to register 
   def new
   end
 
+  # this action intakes the parameters from the form to create a new Customer 
+  def create 
+  end 
 
-  # def login (email, password)
-  #   # Check if email is in database 
-  #   if email_in_database(email)
-  #     # Find Costumer with associated email 
-  #     costumer = Customer.where(email:email)
-  #     # Check if the password matches
-  #     if costumer.password == password
-  #       # Set login status 
-  #       costumer.loginStatus = true
-  #       true
-  #     # If the password is incorrect 
-  #     else
-  #       puts "Password is not correct"
-  #       false
-  #   # Email is not in database 
-  #   else
-  #     puts "Email not registered"
-  #     false
-  #   end 
-  # end
-
-  # def register(firstName, lastName, email, password)
-  #   # Check if the email is in the database
-  #   if email_in_database(email)
-  #     puts "Customer already registered"
-  #     false
-  #   end
-  #   # Email is not already in database 
-  #   else 
-  #     # Create new Customer
-  #     # To do: Check validity of first name 
-  #     # To do: Check validity of last name 
-  #     # To do: Check validity of email 
-  #     # To do: Check validity of password 
-  #     Customer.create(firstName: firstName, lastName: lastName, email: email, password: password)
-  #     # Return true because the registration was successful
-  #     true
-  #   end 
-  # end
-
-  # def email_in_database(email)
-  #   # Checks Customer database and returns true if it exits otherwise false
-  #   inDatabase = Customer.where(email: email).first != nil ? true : false
-  # end
 end
