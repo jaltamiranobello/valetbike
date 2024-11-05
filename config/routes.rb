d8a24290b050fe331ec1a361a77ffd8a66931bb9
mneedham01 <mneedham@smith.edu>
2024-11-04 22:08:12 -0500

    deleted repetitive routes in routes file and outdated actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,16 @@
 Rails.application.routes.draw do
-  get 'customers/update_profile'
   get 'customers/start_trip'
   get 'customers/end_trip'
   get 'customers/past_trip_calculations'
-  get 'directors/login'
-  get 'directors/register'
-  get 'directors/verify_email'
+
   get 'bike/get_bike_id'
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
-  get 'customers/update_profile'
-  get 'customers/start_trip'
-  get 'customers/end_trip'
-  get 'customers/past_trip_calculations'
-  get 'directors/login'
-  get 'directors/register'
-  get 'directors/verify_email'
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
+
   get 'users/verify_account'
   get 'users/delete_account'
+  
   #root to: "stations#index"
   root to: "directors#new"
 end
