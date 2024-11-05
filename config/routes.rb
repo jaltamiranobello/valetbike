9748b935361be359deb1a738d2b9b5718322c1ce
mneedham01 <mneedham@smith.edu>
2024-11-04 22:10:44 -0500

    filled in main view with welcome message, adjusted routes for that to be landing page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'users/verify_account'
   get 'users/delete_account'
-  
+
   #root to: "stations#index"
-  root to: "directors#new"
+  root to: "directors#main"
 end
