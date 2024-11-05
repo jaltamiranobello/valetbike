f6660b0e9957d188104a5f4e387dc5f5bf597d98
mneedham01 <mneedham@smith.edu>
2024-11-04 22:07:47 -0500

    added main action in directors controller and corresponding view

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,10 +1,14 @@
 class DirectorsController < ApplicationController
 
+  # the view for this action is the main page
+  def main 
+  end 
+
   # the view for this action displays the form to register 
   def new
   end
 
-  # this action intakes the parameters from the form to create a new Customer 
+  # intakes the parameters from the form to create a new Customer 
   def create 
   end 
 
