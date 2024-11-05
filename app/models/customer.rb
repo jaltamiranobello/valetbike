f98124ea3d99858f9be1e42f24f5a93232db2c26
mneedham01 <mneedham@smith.edu>
2024-11-04 21:55:59 -0500

    added validations to the Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,16 @@
 class Customer < ApplicationRecord
+    EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+    validates_presence_of   :identifier,
+                            :firstName,
+                            :lastName,
+                            :email,
+                            :password
+    validates_uniqueness_of :identifier,
+                            :email 
+    validates :email, presence: true,
+                    length: { maximum: 100 },
+										uniqueness: true,
+                    format: { with: EMAIL_REGEX },
+                    confirmation: true
 end
