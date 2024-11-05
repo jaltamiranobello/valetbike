b0a006a729aff2a383f80cd62c64a5eadbacace8
mariaigartua <emaildemajo@gmail.com>
2024-11-05 13:30:14 -0500

    Created Card Class

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
-
-  
-
   get 'users/verify_account'
   get 'users/delete_account'
 
