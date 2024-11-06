00e49c0c9910c8f79f8b2bd6f0c5c6c47cce8f5a
mneedham01 <mneedham@smith.edu>
2024-11-05 19:48:13 -0500

    made a method to register user

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,17 +6,19 @@ class DirectorsController < ApplicationController
 
   # the view for this action displays the form to register 
   def new
-    # user = User.create(pass in parameters)
-    # if user.valid? (uses validations)
-      # then set session[:user_id] = user.id
-      # send confirmation message 
-      # re route to main page 
-    # else 
-      # send message that registration was unsuccessful 
   end
 
   # intakes the parameters from the form to create a new Customer 
   def create 
+    user = User.create(params[:customer])
+    if user.valid?
+      session[:user_id] = user.id
+      # change to json messaging in the future 
+      puts "Successful Registration"
+      redirect_to(action: 'main') 
+    else 
+      puts "Unsuccessful Registration" 
+    end 
   end 
-
+  
 end
