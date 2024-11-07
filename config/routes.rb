5826c9722bfe092b89d93450b557fa8bff21ec91
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-11-07 14:52:33 -0500

    Created Home Controller to set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   get 'users/show'
   
-  root to: "home#main"
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
@@ -14,4 +13,5 @@ Rails.application.routes.draw do
   get 'bike/get_battery_percentage'
   get 'users/verify_account'
   get 'users/delete_account'
+  root to: "home#index"
 end
