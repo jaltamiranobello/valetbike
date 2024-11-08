5c35072fe89009ee8948f2a7fc7e71f8cf5e8cd4
mneedham01 <mneedham@smith.edu>
2024-11-08 13:55:13 -0500

    created a Users controller in order to add "show" action to display user info; this is linked on main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'users/shows'
   
   root to: "home#main"
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,7 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get verify_account" do
-    get users_verify_account_url
-    assert_response :success
-  end
-
-  test "should get delete_account" do
-    get users_delete_account_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
