3844d43905c2f55e23480ddcf646cd00e93c9f96
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-11-08 15:41:05 -0500

    deleted repeate devise_for: users

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
-  devise_for :users
   
   root to: "home#index"
 end
