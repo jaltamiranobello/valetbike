86c5e6544bec58711f848f2592c9355b970637ff
mneedham01 <mneedham@smith.edu>
2024-11-10 11:32:09 -0500

    create payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,45 +1,34 @@
-class PaymentsController < ApplicationController
-  # creates a new Payment object 
-  def new
-  end
+Class PaymentsController < ApplicationController
+    def new 
+    end 
 
-  # processes the form from the "new" view, intakes credit card info, 
-  # starts a new trip 
-  def create
+    def create
     #create stripe customer for payment, update if already created
-    # customer = Stripe::Customer.create(
-    #   name: current_user.full_name,
-    #   email: current_user.email,
-    #   description: "Customer id: #{current_user.id}",
-    # )
-    # session = Stripe::Checkout::Session.create( 
-    #   customer: customer, 
-    #   payment_method_types: [‘card’],
-    #   line_items: [{
-    #     price: 'price_1234', #price api id usually starts with price_ApIiD
-    #     quantity: 1,
-    #   }],
-    #   mode: 'subscription',
-    #   success_url:  payments_success_url,
-    #   cancel_url: payments_cancel_url
-    #  )
-    #  redirect_to session.url
+    customer = Stripe::Customer.create(
+      name: current_user.full_name,
+      email: current_user.email,
+      description: "Customer id: #{current_user.id}",
+    )
+    session = Stripe::Checkout::Session.create( 
+      customer: customer, 
+      payment_method_types: [‘card’],
+      line_items: [{
+        price: 'price_1234', #price api id usually starts with price_ApIiD
+        quantity: 1,
+      }],
+      mode: 'subscription',
+      success_url:  payments_success_url,
+      cancel_url: payments_cancel_url
+     )
+     redirect_to session.url
   end
-
-  def edit 
-  end 
-
-  # intakes information from payments edit confirmation page
-  def update 
-  end 
   
-  # def success
-  #   #handle successful payments
-  #   redirect_to root_url, notice: "Purchase Successful"
-  # end
+  def success
+    #handle successful payments
+    redirect_to root_url, notice: "Purchase Successful"
+  end
   
-  # def cancel
-  #   #handle if the payment is cancelled
-  #   redirect_to root_url, notice: "Purchase Unsuccessful"
-  # end
-end
+  def cancel
+    #handle if the payment is cancelled
+    redirect_to root_url, notice: "Purchase Unsuccessful"
+  end
