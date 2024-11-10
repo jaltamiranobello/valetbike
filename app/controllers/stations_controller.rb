6de7b41e780a3a97a25e17ff3df328befcdc42fe
Nafisa Mohamed <nmohamed@smith.edu>
2024-11-10 10:32:56 -0500

    info about bike/station class

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,24 +1,54 @@
 class StationsController < ApplicationController
-  def main 
-  end 
+  
   
   def index
     @stations = Station.all.order(identifier: :asc)
   end
 
-  def has_bike(bike_id)
+  #gets the current_bike id and then displays that bike on the view template
+  def show
+    station_id = params[:station_id].to_i
+    @current_station = Station.find(station_id)
+  end
+  
+  # adds the current bike to the current station the user is at
+  def return_bike
+    
+    if hasbike
+      puts "error"
+ 
+    else
+      @customer_station.docked_bikes<< @current_bike
+    end
 
   end
+  
+  # checks if the customer station has this specific bike  
+  def hasbike
+    station_params
+    return @current_bike.current_station == @customer_station
+  end
+    
+  # removes the customer's bike from the current station they are at
+  def checkout_bike
+    unless hasbike
+      puts "error"
+    else
+      @customer_station.docked_bikes.delete(@current_bike)
 
-  def checkout_bike(bike_id)
-
+    end
   end
 
-  def return_bike(bike_id)
+ 
 
+  # gets the current_station_id and bike_id from the parameters and stores those two values in two instance variables
+  def station_params
+      # params.require(:bike_id)
+      # params.require(:station_id)
+      station_id = params[:station_id].to_i
+      bike_id = params[:bike_id].to_i
+      @customer_station = Station.find(station_id)
+      @current_bike = Bike.find(bike_id)
   end
 
-  def get_list_of_bikes(station_id)
-
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  # adds routes for payments
-  resources :payments
-  post 'payments/create'
-  get 'payments/success'
-  get 'payments/cancel'
-  # adds devise routes for Users 
-  devise_for :users
+  
+  root to: "stations#index"
 
-  get 'users/show'
+  get "stations/show"
+
+
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'bikes/edit'
+  get 'bikes/delete'
+  get 'bikes/new'
   
-  get 'card/remove_card'
-  get 'card/update_payment'
-  get 'card/verify_payment'
-  get 'card/pay'
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
-  
-  root to: "home#main"
+
+#developed this new route to use it later in the html page
+#so that we could display the bikes and the stations in different view pages
+
+
+
 end
