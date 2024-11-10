0b7c19837dabadec9a690ea3b4048eedfc7bdb4f
Nafisa Mohamed <nmohamed@smith.edu>
2024-11-10 10:32:56 -0500

    info about bike/station class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :trips
   # get 'trips/new'
   # get 'trips/create'
@@ -36,5 +37,21 @@ Rails.application.routes.draw do
   get 'users/delete_account'
 
   root to: "home#main"
+
+
+  get "stations/show"
+
+
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'bikes/edit'
+  get 'bikes/delete'
+  get 'bikes/new'
+  
+
+#developed this new route to use it later in the html page
+#so that we could display the bikes and the stations in different view pages
+
+
 
 end
