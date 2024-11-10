eff7d88c934adcb5fbde0e4d69aee70485140870
mneedham01 <mneedham@smith.edu>
2024-11-10 11:32:37 -0500

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,6 @@
-Rails.application.routes.draw do 
-  
-  resources :trips
-  # get 'trips/new'
-  # get 'trips/create'
-
-  get 'card/remove_card'
-  get 'card/update_payment'
-  get 'card/verify_payment'
-  get 'card/pay'
-
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
-
+Rails.application.routes.draw do
   # adds routes for payments
-  resources :payment
+  resources :payments
   post 'payments/create'
   get 'payments/success'
   get 'payments/cancel'
