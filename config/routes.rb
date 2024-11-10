6da40283fd60559707a4eb91708210185fdc8cdd
mneedham01 <mneedham@smith.edu>
2024-11-10 11:32:37 -0500

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
-Rails.application.routes.draw do 
-  
+Rails.application.routes.draw do
+  # adds routes for payments
+  resources :payments
+  post 'payments/create'
+  get 'payments/success'
+  get 'payments/cancel'
+  # adds devise routes for Users 
   devise_for :users
 
-  get "stations/show"
-  get "stations/index"
-  get 'bikes/index'
-  get 'bikes/show'
   get 'users/show'
-  get "stations/show"
-  get "stations/index"
+  
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
   get 'card/pay'
-  get 'trip/index'
-  get 'trip/book'
-  get 'trip/confirm'
-  get 'stations_map/index'
- 
+  get 'bike/get_bike_id'
+  get 'bike/get_station_id'
+  get 'bike/get_station_name'
+  get 'bike/get_battery_percentage'
+  
   root to: "home#main"
-
 end
+
