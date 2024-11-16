b1ca687940bf23e36b8f06ce8367b74c248ca922
mariaigartua <emaildemajo@gmail.com>
2024-11-15 21:31:51 -0500

    Your commit message describing the changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get 'users/show'
+  
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
@@ -7,13 +11,6 @@ Rails.application.routes.draw do
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
-<<<<<<< HEAD
   
   root to: "home#main"
 end
-=======
-  get 'users/verify_account'
-  get 'users/delete_account'
-  root to: "stations#index"
-end
->>>>>>> 79cb473 (Created Bike class and getters)
