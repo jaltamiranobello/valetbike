a5bd6cd973ab16deadd0c62b58faef9aedc3b86c
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-11-17 10:35:56 -0500

    Removed instances of old User Contoller to avoid merge errors when we pull the updated from Devise branch

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_31_182235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "userName"
-    t.string "password"
-    t.boolean "loginStatus"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  
 end
