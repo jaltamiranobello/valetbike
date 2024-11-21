4edd38e9e821c936f12bcd46253beee978edb311
Mackenzie Martin <mmartin@smith.edu>
2024-11-21 15:47:47 -0500

    Pulling payment into branch Mackenzie

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_09_202816) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "trips", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "start_time"
@@ -54,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_09_202816) do
     t.integer "customer_id"
   end
 
->>>>>>> 5487f25 (Built out trip functionality, including start_trip and end_trip methods.)
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
