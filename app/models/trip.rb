e7901502d058ac546a5e404bda97b56808d35f58
Mackenzie Martin <mmartin@smith.edu>
2024-11-22 14:57:57 -0500

    "Updated trip model to have users instaed of customers. Worked on create method in new TripsController."

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,5 +5,5 @@ class Trip < ApplicationRecord
     belongs_to :start_station, class_name: :Station, foreign_key: :start_station_id, optional: false
     belongs_to :end_station, class_name: :Station, foreign_key: :end_station_id, optional: true
     belongs_to :bike_used, class_name: :Bike, foreign_key: :bike_used_id, optional: false
-    belongs_to :customer, class_name: :Customer, foreign_key: :customer_id, optional: false
+    belongs_to :user, class_name: :User, foreign_key: :user_id, optional: false
 end
