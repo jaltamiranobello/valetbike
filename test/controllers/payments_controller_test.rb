70a928c572fabd0d779c1bfc3a4aa9d8aa6f8e3e
mneedham01 <mneedham@smith.edu>
2024-11-22 14:04:01 -0500

    created new payment controller

--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PaymentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get payments_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get payments_create_url
+    assert_response :success
+  end
 end
