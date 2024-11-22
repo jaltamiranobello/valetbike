8a869dbc0e3fb949eeafb9329d2cf3339a6e4969
mneedham01 <mneedham@smith.edu>
2024-11-22 10:39:23 -0500

    added in code from task helper example; changed form to directly link to @trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,17 @@
 class TripsController < ApplicationController
   def new
+    @trip = Trip.new
   end
 
   def create
+    @trip = Trip.new
+    if @trip.save
+      redirect_to trips_path
+    else
+      # The 'new' action is NOT being called here
+      # Assign any instance variables needed
+      # @count = Task.count
+      render('new')
+    end
   end
 end
