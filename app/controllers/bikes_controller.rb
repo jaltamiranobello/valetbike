8c46e7ca6023afceaa8938033c5af1a9d889901a
Nafisa Mohamed <nmohamed@smith.edu>
2024-11-21 21:29:42 -0500

    Added the bike methods in the controller class

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,16 +1,18 @@
 class BikesController < ApplicationController
   def index
+    @bikes = Bike.all
   end
 
   def show
+    @bike = Bike.find(params[:id])
   end
 
   
   def edit
+    @bike = Bike.find(params[:id])
   end
 
-  def update
-  end
+  
 
   
 end
