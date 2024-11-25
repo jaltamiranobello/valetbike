eb7ba73d1dca8959f854cf8087d7b6c0a8c917c2
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-11-25 15:38:34 -0500

    Updated the has_many/belongs_to relationship between the bikes and stations to match by the station identifier rather than the station id to correctly ensure the database matches the csv file

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,5 +3,5 @@ class Bike < ApplicationRecord
   validates_uniqueness_of  :identifier
   
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
-  has_many :trips_taken, class_name: :Trip, foreign_key: :bike_used_id
+  
 end
