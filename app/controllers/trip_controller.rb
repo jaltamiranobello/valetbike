4ede17c386a0a53dfe5dcb2ab7dfa6f2f2e2b889
Nafisa Mohamed <nmohamed@smith.edu>
2024-11-27 16:34:44 -0500

    intial steps on updating the trip controller

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,39 +1,57 @@
-#Need to make sure the trip works with the stations and bikes
-class TripController < ApplicationController
-  helper_method :start_trip, :end_trip, :get_current_length_of_active_trip, :calculate_duration, :calculate_emmisions
-
-  def index
-    @trips = Trip.all.order(identifier: :desc)
+class TripsController < ApplicationController
+  def new
+    @trip = Trip.new
   end
 
-  def start_trip(start_station_id, bike_used_id, customer_id)
+  def create
+    @trip = Trip.new
+    trip_success = initalize_trip(params[:station_id].to_i, params[:bike_id].to_i, current_user.id)
+    unless trip_success == nil
+      if @trip.save 
+        redirect_to trips_path
+      end
+    else
+      # The 'new' action is NOT being called here
+      # Assign any instance variables needed
+      # @count = Task.count
+      render('new')
+    end
+  end
+
+
+  def initalize_trip(start_station_id, bike_used_id, user_id)
     #validate bike, station & customer
-    unless real_bike_id?(bike_used_id)
+    unless Bike.exists?(bike_used_id)
       puts "No bike w/ id"
       return nil
     end
-    unless real_station_id?(start_station_id)
+    unless Station.exits?(start_station_id)
       puts "No station w/ id"
       return nil
     end
-    unless Customer.exists?(start_station_id)
-      puts "No customer w/ id"
+    unless User.exists?(start_station_id)
+      puts "No user w/ id"
       return nil
     end
 
     bike_used = Bike.find(bike_used_id)
     start_station = Station.find(start_station_id)
+    user = User.find(user_id)
 
     #check bike @ station
     if bike_used.current_station == start_station
       #figure out start time
-      start_time = time_in_minutes
+      #start_time = time_in_minutes
 
       #find next id
       id = Trip.maximum(:id).next
 
-      #create trip
-      trip = Trip.create(id: id, identifier: id, start_time: start_time, start_station_id: start_station_id, bike_used_id: bike_used_id, customer_id: customer_id)
+      #set variables trip
+      # trip = Trip.create(id: id, identifier: id, start_time: start_time, start_station_id: start_station_id, bike_used_id: bike_used_id, customer_id: customer_id)
+      @trip.id = id
+      @trip.start_station = start_station
+      @trip.bike_used = bike_used
+      @trip.user = user_id
 
       #checkout bike
       bike_used.current_station = nil
@@ -78,45 +96,6 @@ class TripController < ApplicationController
     trip
   end
 
-  def get_current_length_of_active_trip(trip_id)
-    #validate trip
-    unless real_trip_id?(trip_id)
-      puts "No trip w/ id"
-      return nil
-    end
-
-    start_time = Trip.find(trip_id).start_time
-    current_time = time_in_minutes
-    diffrence_in_times(start_time, current_time)
-  end
-
-  def calculate_duration(trip_id)
-    #validate trip
-    unless real_trip_id?(trip_id)
-      puts "No trip w/ id"
-      return nil
-    end
-
-    trip = Trip.find(trip_id)
-    diffrence_in_times(trip.start_time, trip.end_time)
-  end
-
-  def calculate_emmisions(trip_id)
-    #validate trip
-    unless real_trip_id?(trip_id)
-      puts "No trip w/ id"
-      return nil
-    end
-
-    time_in_minutes = calculate_duration(trip_id)
-    milage = 0.25*time_in_minutes #calculates the milage assuming an average speed of 15mph or 0.25miles/minute
-    411*milage #calculates the emmisions in grams of Co2 for the milage in an average car (produces 411gr per mile)
-  end
-
-  def trip_params
-    params.expect(:trip_id, :station_id, :bike_id)
-  end
-
   #returns the number of minutes elapsed since the start of the current day.
   def time_in_minutes
     time = Time.new
@@ -127,24 +106,4 @@ class TripController < ApplicationController
     (hours_elapsed * 60) + minutes_elapsed
   end
 
-  #returns the diffrence in minutes between two times expressed as the number of minutes elapsed in the day. (asumes times are <24 apart)
-  def diffrence_in_times(first_time, second_time)
-    if first_time < second_time
-      second_time - first_time
-    else 
-      second_time + 1440 - first_time
-    end
-  end
-
-  def real_station_id?(station_id)
-    Station.exists?(station_id)
-  end
-
-  def real_bike_id?(bike_id)
-    Bike.exists?(bike_id)
-  end
-
-  def real_trip_id?(trip_id)
-    Trip.exists?(trip_id)
-  end
 end
