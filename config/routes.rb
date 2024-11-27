16eae2a0a3c95eec386b1573bf899d681caf574c
Nafisa Mohamed <nmohamed@smith.edu>
2024-11-27 15:45:01 -0500

    added the stationmap routes to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  
+  devise_for :users
+
+  get "stations/show"
+  get "stations/index"
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'users/show'
+  get "stations/show"
+  get "stations/index"
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
   get 'card/pay'
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
-  get 'users/verify_account'
-  get 'users/delete_account'
-
+  get 'trip/index'
+  get 'trip/book'
+  get 'trip/confirm'
+  get 'stations_map/index'
+ 
   root to: "home#main"
 
 end
