d9923efb391626d914577945a562d3ca00f10f92
mneedham01 <mneedham@smith.edu>
2024-11-28 10:36:00 -0500

    added comments to payments controller and views

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,14 +1,19 @@
 class PaymentsController < ApplicationController
+  # the view for the new action will intake the CC infor 
   def new
   end
 
+  # this action will process the info and start a new transaction
   def create 
   end
 
+  # the view for this action will be a confirmation page for ending trip 
+  # and charging card 
   def edit
   end
 
+  # this action will process the end of the transaction
   def update
   end
-  
+
 end
