1ba10513d53d834d215b83eeebd3fda38cc94699
mneedham01 <mneedham@smith.edu>
2024-11-28 10:28:13 -0500

    created payments controller, new and edit placeholder views, and edited routes file

--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -6,8 +6,13 @@ class PaymentsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+<<<<<<< HEAD
   test "should get create" do
     get payments_create_url
+=======
+  test "should get edit" do
+    get payments_edit_url
+>>>>>>> ca346d1 (created payments controller, new and edit placeholder views, and edited routes file)
     assert_response :success
   end
 end
