c136788222ec02629b6e7041121a51b630f6e436
mneedham01 <mneedham@smith.edu>
2024-11-29 13:51:10 -0500

    deleted payment and card controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   # adds routes for payments
-  resources :payments
-  post 'payments/create'
-  get 'payments/success'
-  get 'payments/cancel'
   # adds devise routes for Users 
   devise_for :users
 
   get 'users/show'
+
   get 'stations/show'
   get 'stations/index'
+
   get 'bikes/index'
   
 
@@ -18,6 +16,7 @@ Rails.application.routes.draw do
   get 'card/update_payment'
   get 'card/verify_payment'
   get 'card/pay'
+  
   get 'bike/get_bike_id'
   get 'bike/get_station_id'
   get 'bike/get_station_name'
