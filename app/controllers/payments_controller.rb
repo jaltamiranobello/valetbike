5fccae7d141107719a646c4ad838f0584ce2d108
mneedham01 <mneedham@smith.edu>
2024-11-30 20:57:58 -0500

    added code from stripe example to create function

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,24 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    #create stripe customer for payment, update if already created
+    customer = Stripe::Customer.create(
+      name: current_user.full_name,
+      email: current_user.email,
+      description: "Customer id: #{current_user.id}",
+    )
+    session = Stripe::Checkout::Session.create( 
+      customer: customer, 
+      payment_method_types: [‘card’],
+      line_items: [{
+        price: 'price_1234', #price api id usually starts with price_ApIiD
+        quantity: 1,
+      }],
+      mode: 'subscription',
+      success_url:  payments_success_url,
+      cancel_url: payments_cancel_url
+     )
+     redirect_to session.url
   end
 
   def edit
