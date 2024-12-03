56fa029a9636074e7ba30d882506350339e88fef
mneedham01 <mneedham@smith.edu>
2024-12-03 15:57:11 -0500

    new code for #new action that takes in a price, makes a new payment link, and sends it in to the new view

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,27 +1,24 @@
 class PaymentsController < ApplicationController
   def new
+    # create a new link 
+    tripPrice = 1000
+    # create price object
+    priceObject = Stripe::Price.create({
+      currency: 'usd',
+      unit_amount: tripPrice,
+      product_data: {name: 'Trip'},
+    })
+    @paymentLink = Stripe::PaymentLink.create({
+      line_items: [
+        {
+          price: priceObject,
+          quantity: 1
+        },
+      ],
+    })
   end
 
   def create
-    # create new customer
-    customer = Stripe::Customer.create(
-      name: current_user.first_name + " " + current_user.first_name, 
-      email: current_user.email,
-      description: "Customer id: #{current_user.id}",
-    )
-    # create new payment session
-    session = Stripe::Checkout::Session.create( 
-      customer: customer, 
-      payment_method_types: [‘card’],
-      line_items: [{
-        price: 'price_1234', #price api id usually starts with price_ApIiD
-        quantity: 1,
-      }],
-      mode: 'subscription',
-      success_url:  payments_success_url,
-      cancel_url: payments_cancel_url
-     )
-     redirect_to session.url
   end
 
   def edit
