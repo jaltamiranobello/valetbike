07d6911da560324483d46d869465488a2934a1d1
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-12-03 15:32:57 -0500

    updated the trips model to include the price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_09_202527) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_03_201618) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -49,6 +49,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_09_202527) do
     t.integer "start_station_id"
     t.integer "end_station_id"
     t.integer "bike_used_id"
+    t.integer "user_id"
+    t.float "price"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -60,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_09_202527) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "firstName"
+    t.string "lastName"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- lib/tasks/import_stations.rake
+++ lib/tasks/import_stations.rake
@@ -20,5 +20,5 @@ namespace :db do
             end
     
         end
-
+    end
 end
