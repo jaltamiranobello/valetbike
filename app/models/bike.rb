349b44d5902c0c8cd843217ecc16d912d4860a3b
Nafisa Mohamed <nmohamed@smith.edu>
2024-12-02 19:31:26 -0500

    function to find the current station that a user rented a bike from

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -5,4 +5,7 @@ class Bike < ApplicationRecord
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, primary_key: :identifier,  optional: true
   has_many :trips_taken, class_name: :Trip, foreign_key: :bike_used_id, primary_key: :identifier
   
+  def current_station
+    Station.find_by(identifier: current_station_id)
+  end
 end
