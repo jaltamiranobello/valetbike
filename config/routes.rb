4057e98551cb97e7a6768cbd9888518b7ea237be
mariaigartua <emaildemajo@gmail.com>
2024-12-03 13:45:54 -0500

    im trying to pull stations

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,23 @@
-Rails.application.routes.draw do
-  # adds routes for payments
-  resources :payments
-  post 'payments/create'
-  get 'payments/success'
-  get 'payments/cancel'
-  # adds devise routes for Users 
+Rails.application.routes.draw do 
+  
   devise_for :users
 
+  get "stations/show"
+  get "stations/index"
+  get 'bikes/index'
+  get 'bikes/show'
   get 'users/show'
-  
+  get "stations/show"
+  get "stations/index"
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
   get 'card/pay'
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
-<<<<<<< HEAD
-  
-  get 'users/verify_account'
-  get 'users/delete_account'
+  get 'trip/index'
+  get 'trip/book'
+  get 'trip/confirm'
   get 'stations_map/index'
-
+ 
   root to: "home#main"
-
-
-  get "stations/show"
-  get "stations/index"
-
-  
-
-
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bikes/edit'
-  get 'bikes/delete'
-  get 'bikes/new'
-  
-
-#developed this new route to use it later in the html page
-#so that we could display the bikes and the stations in different view pages
-
-
 
 end
