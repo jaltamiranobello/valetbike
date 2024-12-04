a544fd43d332c76e56b3c1ca01be9122e372b857
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-12-03 20:27:34 -0500

    Removed card migration since not needed and added parametere to the trips/create view

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,6 +6,6 @@ class Trip < ApplicationRecord
     belongs_to :end_station, class_name: :Station, foreign_key: :end_station_id, primary_key: :identifier, optional: true
     belongs_to :bike_used, class_name: :Bike, foreign_key: :bike_used_id, primary_key: :identifier, optional: false
     belongs_to :user, class_name: :User, foreign_key: :user_id, primary_key: :id, optional: false
-    belongs_to :card, class_name: :Card, foreign_key: :card_id, primary_key: :id, optional: false
+    # belongs_to :card, class_name: :Card, foreign_key: :card_id, primary_key: :id, optional: false
 
 end
