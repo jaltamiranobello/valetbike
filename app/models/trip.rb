112ff98b7734a9b5487cbab667220f67d341e76f
Fernanda Altamirano <jaltamiranobello@smith.edu>
2024-12-04 10:32:43 -0500

    Updated the create method to instantiate a trip object and needed to delete identifier in trip model as there is no identifier needed

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
-    validates_presence_of   :identifier
-    validates_uniqueness_of :identifier
+    # validates_presence_of   :identifier
+    # validates_uniqueness_of :identifier
 
     belongs_to :start_station, class_name: :Station, foreign_key: :start_station_id, primary_key: :identifier, optional: false
     belongs_to :end_station, class_name: :Station, foreign_key: :end_station_id, primary_key: :identifier, optional: true
