881573b635e3e259a46e1860253b34cb5b92896c
Nafisa Mohamed <nmohamed@smith.edu>
2024-12-04 19:07:43 -0500

    added an identifier functionality as well as allowing the trip id to increament by one each time we rent

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,21 @@
 class Trip < ApplicationRecord
     # validates_presence_of   :identifier
     # validates_uniqueness_of :identifier
+    validates_presence_of  :bike_used_id,
+                           :start_station_id,
+                           :user_id
+                          
 
     belongs_to :start_station, class_name: :Station, foreign_key: :start_station_id, primary_key: :identifier, optional: false
     belongs_to :end_station, class_name: :Station, foreign_key: :end_station_id, primary_key: :identifier, optional: true
     belongs_to :bike_used, class_name: :Bike, foreign_key: :bike_used_id, primary_key: :identifier, optional: false
     belongs_to :user, class_name: :User, foreign_key: :user_id, primary_key: :id, optional: false
-    # belongs_to :card, class_name: :Card, foreign_key: :card_id, primary_key: :id, optional: false
+    
+    before_create :set_identifier
 
+    private
+
+    def set_identifier
+        self.identifier = self.bike_used.identifier if self.bike_used
+    end
 end
