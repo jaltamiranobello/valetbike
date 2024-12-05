2326b958becd0e44678de7434450ad9231992ae4
Nafisa Mohamed <nmohamed@smith.edu>
2024-12-04 22:02:03 -0500

    deleted uncessary functions

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,15 +1,4 @@
 class BikesController < ApplicationController
-  def get_bike_id
-    self.bike_id
-  end
-
-  def get_station_id
-    self.station_id
-  end
-
-  def get_station_name
-    self.station
-  end
   
   def index
     @bikes = Bike.all
@@ -19,12 +8,6 @@ class BikesController < ApplicationController
     @bike = Bike.find_by(current_station_id: params[:current_station_id])
   end
 
-  
-  def edit
-    @bike = Bike.find(params[:id])
-  end
-
-  
 
   
 end
