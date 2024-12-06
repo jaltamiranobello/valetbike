bfe9697f140c25360cf38b70b82d96ec7d65ed05
mariaigartua <emaildemajo@gmail.com>
2024-12-06 02:51:51 -0500

    fixed merging changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,40 @@
 Rails.application.routes.draw do
-  get 'trips/create', to: 'trips#create', as:'create_trip'
-  resources :payments 
-  resources :trips
+  # Resourceful routes
+  resources :payments
+  resources :trips, except: [:create] # Explicitly exclude :create since we define it manually
+
+  # Custom trip routes
+  get 'trips/create', to: 'trips#create', as: 'create_trip'
   get 'trips/index'
   get 'trips/new'
-  # get 'trips/create'
 
+  # Card-related routes
   get 'card/remove_card'
   get 'card/update_payment'
   get 'card/verify_payment'
   get 'card/pay'
 
+  # Bike-related routes
   get 'bike/get_bike_id'
   get 'bike/get_station_id'
   get 'bike/get_station_name'
   get 'bike/get_battery_percentage'
-  # get 'trips/new'
 
-  
-  #post 'payments/create'
-  # get 'payments/success'
-  # get 'payments/cancel'
-  # adds devise routes for Users 
+  # Devise routes for users
   devise_for :users
-
   get 'users/show'
-  
-  get 'card/remove_card'
-  get 'card/update_payment'
-  get 'card/verify_payment'
-  get 'card/pay'
-  get 'bike/get_bike_id'
-  get 'bike/get_station_id'
-  get 'bike/get_station_name'
-  get 'bike/get_battery_percentage'
-  
   get 'users/verify_account'
   get 'users/delete_account'
 
-  root to: "home#main"
+  # Station routes
+  get 'stations/show'
+  get 'stations/index'
 
+  # Bike routes
+  get 'bikes/index'
+  get 'bikes/show'
+
+  # Root route
+  root to: 'home#main'
 end
+
